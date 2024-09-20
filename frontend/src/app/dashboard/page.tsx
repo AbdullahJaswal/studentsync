cea8e11003dd67102f9de4a9075073743f595706
Abdullah Jaswal <abdullah.abid95@gmail.com>
2024-09-21 03:07:09 +1000

    fixed merge conflicts

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,23 +1,12 @@
-import {getServerSession} from "next-auth";
-import {authOptions} from "@/app/api/auth/[...nextauth]/route";
-import type {Metadata} from "next";
-import {AuthSessionType} from "@/types/auth/authsession";
+import type { Metadata } from "next";
+import DashboardPosts from "@/components/dashboard/posts";
 
 export const metadata: Metadata = {
   title: "Dashboard - StudentSync",
-  description: "StudentSync is a platform for students to manage their academic life.",
+  description:
+    "StudentSync is a platform for students to manage their academic life.",
 };
 
 export default async function Dashboard() {
-  const session: AuthSessionType | null = await getServerSession(authOptions);
-
-  if (!session) {
-    return <div>You are not logged in</div>;
-  }
-
-  return (
-    <div>
-      <h1>Logged in as {session.user.email}</h1>
-    </div>
-  );
+  return <DashboardPosts />;
 }
