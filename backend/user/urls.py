3b95aee7c63f820a59b7d41964c6def42690665f
Ruchdi Muttaqin <ruchdi.muttaqin@esdm.go.id>
2024-09-20 21:50:38 +1000

    add dummy user/icsparser/ to backend

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import DeleteAccount
+from .views import DeleteAccount, IcsParser
 
 app_name = "user"
 
 urlpatterns = [
     # User
     path("delete/", DeleteAccount.as_view(), name="delete"),
+    path("icsparser/", IcsParser.as_view(), name="icsparser"),
 ]
